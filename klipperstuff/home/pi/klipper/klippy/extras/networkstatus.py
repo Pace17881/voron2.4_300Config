674f5013d69aec2b9c663ce699cbb9b01720ad19
Sebastian Polster <polsterseb@protonmail.com>
2023-05-18 00:41:15 +0200

    separate leds, updated python

--- klipperstuff/home/pi/klipper/klippy/extras/networkstatus.py
+++ klipperstuff/home/pi/klipper/klippy/extras/networkstatus.py
@@ -1,4 +1,5 @@
-import logging, commands
+import logging
+import subprocess
 
 REPORT_TIME = 15.0
 RPI_NETWORK_INTERFACE = "wlan0"
@@ -7,11 +8,10 @@ class NetworkStatus:
     def __init__(self, config):
         self.printer = config.get_printer()
         self.reactor = self.printer.get_reactor()
-        
+
         self.interface = config.get("interface", RPI_NETWORK_INTERFACE)
 
-        self.printer.register_event_handler("klippy:connect",
-                                            self.handle_connect)
+        self.printer.register_event_handler("klippy:connect", self.handle_connect)
         self.sample_timer = self.reactor.register_timer(self._get_pi_networkinfo)
 
     def handle_connect(self):
@@ -19,8 +19,11 @@ class NetworkStatus:
 
     def _get_pi_networkinfo(self, eventtime):
         try:
-            self.hostname = commands.getoutput("hostname")
-            self.ip = commands.getoutput("/sbin/ifconfig " + RPI_NETWORK_INTERFACE + " | grep -i mask | awk '{print $2}'| cut -f2 -d:")
+            self.hostname = subprocess.check_output(['hostname']).decode().strip()
+            ifconfig_output = subprocess.check_output(['/sbin/ifconfig', RPI_NETWORK_INTERFACE])
+            self.ip = ifconfig_output.decode().split('\n')[1].split()[1][5:]
+        except subprocess.CalledProcessError as e:
+            logging.exception("networkstatus: Error running command: %s", e)
         except Exception:
             logging.exception("networkstatus: Error reading data")
 
